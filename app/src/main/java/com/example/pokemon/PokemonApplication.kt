3d74aa337fb3a0625fc95cae76d04983c2d35c7b
beppe <longo.beppe@gmail.com>
2020-11-24 20:12:37 +0100

    add koin and data\domain empty module

---
+++ app/src/main/java/com/example/pokemon/PokemonApplication.kt
@@ -0,0 +1,21 @@
+package com.example.pokemon
+
+import android.app.Application
+import org.koin.core.context.startKoin
+import org.koin.android.ext.koin.androidContext
+
+public class PokemonApplication : Application() {
+
+    override fun onCreate(){
+        super.onCreate()
+
+        startKoin {
+            printLogger()
+            // declare used Android context
+            androidContext(this@PokemonApplication)
+            // declare modules
+            // modules(myModule)
+        }
+    }
+
+}
