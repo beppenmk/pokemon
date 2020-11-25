85e104c9fad070563920bf07dcc1d69e1f1c3234
beppe <longo.beppe@gmail.com>
2020-11-25 11:25:46 +0100

    first test MVI

--- app/src/main/java/com/example/pokemon/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/pokemon/ui/view/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.pokemon.ui.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
 import com.example.pokemon.R
 import com.example.pokemon.ui.viewmodel.TestViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import org.koin.androidx.viewmodel.ext.android.getViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -12,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var testViewModel: TestViewModel
     private lateinit var testBtn: Button
+    private lateinit var nameTv: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,10 +24,26 @@ class MainActivity : AppCompatActivity() {
 
         testViewModel = getViewModel()
         testBtn = findViewById(R.id.test_btn)
+        nameTv = findViewById(R.id.name_tv)
+
+        testViewModel.state.onEach {
+            state ->  handleState(state)
+        }.launchIn( lifecycleScope)
+
+
 
         testBtn.setOnClickListener {
-            testViewModel.getTest()
+          //  testViewModel.getTest()
+            testViewModel.intentChannel.offer(TestViewModel.Intent.GetTest)
         }
 
+    }
+
+      private fun handleState(state: TestViewModel.State) {
+        when(state){
+            TestViewModel.State.Idle -> Unit
+            TestViewModel.State.TestRecived -> nameTv.text = state.toString()
+
+        }
     }
 }
--- app/src/main/java/com/example/pokemon/ui/viewmodel/TestViewModel.kt
+++ app/src/main/java/com/example/pokemon/ui/viewmodel/TestViewModel.kt
@@ -1,16 +1,65 @@
 package com.example.pokemon.ui.viewmodel
 
-import android.util.Log
-import androidx.lifecycle.ViewModel
-import com.example.usecase.GetTestUseCase
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pokemon.ui.viewmodel.TestViewModel.State.TestRecived
+import com.example.usecase.GetTestUseCase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalCoroutinesApi::class)
 class TestViewModel(
-   private val getTestUseCase: GetTestUseCase
+   private val getTestUseCase: GetTestUseCase,
+
 ) : ViewModel() {
 
-    fun getTest(): Any {
+    val intentChannel = Channel<Intent>(Channel.UNLIMITED)
+    private val _state = MutableStateFlow<State>(State.Idle)
+    val state: StateFlow<State>
+        get() = _state
+
+    sealed class Intent {
+        object GetTest : Intent()
+    }
+
+    sealed class State {
+
+        object Idle : State()
+        object TestRecived : State()
+    }
+
+
+    init {
+        viewModelScope.launch {
+            handleIntents()
+        }
+    }
+
+
+    suspend fun handleIntents(){
+        intentChannel.consumeAsFlow().collect { intent ->
+            when (intent) {
+                Intent.GetTest -> {
+                    _state.value = TestRecived
+                }
+            }
+        }
+
+    }
+
+
+
+
+    /***********************/
+   /* fun getTest(): Any {
         var response = getTestUseCase.invoke()
         Log.d("TAG", "TEST=>$response")
         return   response
-    }
+    }*/
 }
